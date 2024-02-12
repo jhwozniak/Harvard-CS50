9c1d94515ce54cd890f5bbfb72f07c9be1867d12
jhwozniak <118801378+jhwozniak@users.noreply.github.com>
2024-02-12 14:51:45 +0100

    Add files via upload

---
+++ taqueria/taqueria.py
@@ -0,0 +1,27 @@
+#prompts user for order from menu and displays total cost
+import sys
+
+menu = {
+    "Baja Taco": 4.00,
+    "Burrito": 7.50,
+    "Bowl": 8.50,
+    "Nachos": 11.00,
+    "Quesadilla": 8.50,
+    "Super Burrito": 8.50,
+    "Super Quesadilla": 9.50,
+    "Taco": 3.00,
+    "Tortilla Salad": 8.00
+}
+
+total_cost = 0
+while True:
+    try:
+        item = input("Item: ")
+        item = item.title()
+        if item in menu:
+            total_cost += menu[item]
+            print("Total: $" + "%.2f" % total_cost)
+    except EOFError:
+        print()
+        sys.exit(0)
+
